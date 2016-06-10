e65659c171991ca234e4e5645a0f3fa706a00dd0
Granger <GrangerAbuhoff@gmail.com>
2016-06-10 18:20:58 -0400

    add items to lists with ember Object

---
+++ app/components/item-list.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+var firstItem = Ember.Object.create({name: "first item", description: "---"});
+
+export default Ember.Component.extend({
+  items: [firstItem],
+  actions: {
+    addItem: function(){
+      let itemName = this.get('itemName');
+      let itemDescription = this.get('itemDescription');
+      let item = Ember.Object.create({name: itemName, description: itemDescription})
+      this.get('items').pushObject(item);
+    }
+  }
+});
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  lists: ["polar", "brown"],
-  actions: {
-    addList: function(){
-      let listName = this.get('listName');
-      this.get('lists').pushObject(listName);
-    }
-  }
 });
