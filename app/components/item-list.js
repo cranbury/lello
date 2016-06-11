f1fb418cc0aaad3080164956aaa74c0b71744adf
Granger <GrangerAbuhoff@gmail.com>
2016-06-11 11:17:27 -0400

    unique state for lists

--- app/components/item-list.js
+++ app/components/item-list.js
@@ -1,15 +1,15 @@
 import Ember from 'ember';
 
-var firstItem = Ember.Object.create({name: "first item", description: "---"});
+// var firstItem = Ember.Object.create({name: "first item", description: "---"});
 
 export default Ember.Component.extend({
-  items: [firstItem],
+  // items: [firstItem],
   actions: {
     addItem: function(){
       let itemName = this.get('itemName');
       let itemDescription = this.get('itemDescription');
       let item = Ember.Object.create({name: itemName, description: itemDescription})
-      this.get('items').pushObject(item);
+      this.get('list').items.pushObject(item);
     }
   }
 });
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
 
-var blankList = Ember.Object.create({title: "Default List"});
+var defaultTitle = "Default List";
+var firstItem = Ember.Object.create({name: "first item", description: "---"});
+var blankList = Ember.Object.create({title: defaultTitle, items: [firstItem]});
 
 export default Ember.Controller.extend({
-  lists: [blankList]
+  lists: [blankList],
+  actions: {
+    addList: function(){
+      var blankItem = Ember.Object.create({name: "first item", description: "---"});
+      let newList = Ember.Object.create({title: defaultTitle, items: [blankItem]});
+      this.get("lists").pushObject(newList);
+    }
+  }
 });
