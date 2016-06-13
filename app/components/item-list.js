b8229095edd14df8bcd6a9f0c8d9f1abd65d427d
Granger <GrangerAbuhoff@gmail.com>
2016-06-13 00:46:25 -0400

    remove event listener

--- app/components/item-list.js
+++ app/components/item-list.js
@@ -3,13 +3,41 @@ import Ember from 'ember';
 // var firstItem = Ember.Object.create({name: "first item", description: "---"});
 
 export default Ember.Component.extend({
-  // items: [firstItem],
+  isExpanded: false, //default state
   actions: {
-    addItem: function(){
+
+    expand: function() {
+      this.set('isExpanded', true);
+      var comp = this;
+      var doIt = function(){console.log("OOO"); comp.set('isExpanded', false); comp.sendAction("addItem")};
+
+      document.addEventListener('mouseup', function clickAwayListener(e){
+        var container = $(".fill");
+
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+          doIt();
+          document.removeEventListener('mouseup', clickAwayListener, true);
+        }
+      }, true);
+
+      // document.addEventListener('mouseup', clickAwayListener, true);
+    },
+
+    addItem: function()
+    {
+      console.log("hit");
       let itemName = this.get('itemName');
-      let itemDescription = this.get('itemDescription');
-      let item = Ember.Object.create({name: itemName, description: itemDescription})
+      // let itemDescription = this.get('itemDescription');
+      let item = Ember.Object.create({name: itemName, description: itemDescription});
       this.get('list').items.pushObject(item);
-    }
+      this.set('isExpanded', false);
+    },
+
+    watchIsExpanded: function() {
+      console.log("hitter");
+      this.action('addItem');
+    }.observes('isExpanded').on('init')
   }
 });
